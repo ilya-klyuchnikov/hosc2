849b821cedde5d7a5e6f89df72b50933fe0da933
Ilya Klyuchnikov <ilya.klyuchnikov@gmail.com>
2010-07-09 15:23:14 +0400

    simplifications

--- main/hosc/lang/io/HsParsers.scala
+++ main/hosc/lang/io/HsParsers.scala
@@ -12,7 +12,7 @@ object HsParsers extends HsTokenParsers with PackratParsers with ImplicitConvers
 	 
 	lazy val expr: PackratParser[Expr] = chainl1(head, arg, success(App(_, _)))
 	lazy val lam:  PackratParser[Lam] = "\\" ~> vrb ~ expr ^^ Lam
-	lazy val vrb:  PackratParser[Var] = lId ^^ Var
+	lazy val vrb:  PackratParser[Var] = ident ^^ Var
 	
 	lazy val head = vrb | lam | "(" ~> expr <~ ")"
 	lazy val arg = head | "(" ~> expr <~ ")"
@@ -21,23 +21,12 @@ object HsParsers extends HsTokenParsers with PackratParsers with ImplicitConvers
 }
 
 class HsTokenParsers extends StdTokenParsers {
-	type Tokens = HsTokens
+	type Tokens = StdTokens
 	val lexical = new HsLexical
-	def uId = elem("identifier", _.isInstanceOf[lexical.UId]) ^^ (_.chars)
-	def lId = elem("identifier", _.isInstanceOf[lexical.LId]) ^^ (_.chars)  
-}
-
-trait HsTokens extends StdTokens {
-	case class LId(chars: String) extends Token
-	case class UId(chars: String) extends Token
 }
 
 import scala.util.parsing.input.CharArrayReader.EofCh
-class HsLexical extends StdLexical with HsTokens { s: Parsers => 
-	implicit def flatten[L](l: String => L) = (p: ~[s.Elem, List[s.Elem]]) => l(p._1 :: p._2 mkString)
-	override def token = uLetter ~ rep(letter | digit) ^^ UId | lLetter ~ rep(letter | digit) ^^ LId | super.token
-	def uLetter = elem("upper-letter", _.isUpper)
-	def lLetter = elem("lower-letter", _.isLower)
+class HsLexical extends StdLexical { s: Parsers => 
 	override protected def comment = '-' ~ '}'  ^^ { _ => ' '  } | chrExcept(EofCh) ~ comment
 	override def whitespace = 
 		rep(whitespaceChar | '{' ~ '-' ~ comment | '-' ~ '-' ~ rep(chrExcept(EofCh, '\n')) | '{' ~ '-' ~ failure("unclosed comment"))
